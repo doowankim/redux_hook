aa725d188449fdb9abbe291ee12db8d61b9fa860
Doowankim <rlaendhks11@gmail.com>
2020-06-06 23:46:08 +0900

    feat: panel color change

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '../../container/Card';
-import styled from 'styled-components';
 
 const Home = () => {
   return (
--- src/container/Card/index.js
+++ src/container/Card/index.js
@@ -1,71 +1,86 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useState} from 'react';
+import styled, {keyframes} from 'styled-components';
 
-class Card extends React.Component {
-  state = {
-    color: 'white',
-    color2: 'white',
-  };
-  onChange = () => {
-    this.setState({color: '#495057'});
-  };
-  onChange2 = () => {
-    this.setState({color2: '#495057'});
-  };
-  render() {
-    return (
-      <>
-        <Layout>
-          <Box>
-            <Panel
-              style={{backgroundColor: this.state.color}}
-              onClick={this.onChange}
-            >
-              <PanelTop>
-                <PanelCircle />
-                <PanelInfo>
-                  <PanelName>이름을 적어주세요</PanelName>
-                  <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
-                </PanelInfo>
-              </PanelTop>
-            </Panel>
-            <Panel
-              style={{backgroundColor: this.state.color2}}
-              onClick={this.onChange2}
-            >
-              <PanelTop>
-                <PanelCircle />
-                <PanelInfo>
-                  <PanelName>이름을 적어주세요</PanelName>
-                  <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
-                </PanelInfo>
-              </PanelTop>
-            </Panel>
-            <Panel>
-              <PanelTop>
-                <PanelCircle />
-                <PanelInfo>
-                  <PanelName>이름을 적어주세요</PanelName>
-                  <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
-                </PanelInfo>
-              </PanelTop>
-            </Panel>
-          </Box>
+const Card = () => {
+  const [isClick, setClick] = useState(true);
+  const onClickChange = () => setClick(!isClick);
+  console.log(isClick);
+  return (
+    <div>
+      <Layout>
+        <Box>
+          <Panel onClick={onClickChange}>
+            <PanelTop>
+              <PanelCircle />
+              <PanelInfo>
+                <PanelName>이름을 적어주세요</PanelName>
+                <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
+              </PanelInfo>
+            </PanelTop>
+          </Panel>
 
-          <Box>
-            <div style={{fontSize: '14px'}}>확인 목록이 없습니다.</div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </Box>
-        </Layout>
-      </>
-    );
+          <Panel>
+            <PanelTop>
+              <PanelCircle />
+              <PanelInfo>
+                <PanelName>이름을 적어주세요</PanelName>
+                <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
+              </PanelInfo>
+            </PanelTop>
+          </Panel>
+          <Panel>
+            <PanelTop>
+              <PanelCircle />
+              <PanelInfo>
+                <PanelName>이름을 적어주세요</PanelName>
+                <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
+              </PanelInfo>
+            </PanelTop>
+          </Panel>
+          <Panel>
+            <PanelTop>
+              <PanelCircle />
+              <PanelInfo>
+                <PanelName>이름을 적어주세요</PanelName>
+                <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
+              </PanelInfo>
+            </PanelTop>
+          </Panel>
+          <Panel>
+            <PanelTop>
+              <PanelCircle />
+              <PanelInfo>
+                <PanelName>이름을 적어주세요</PanelName>
+                <PanelDetail>성별 / 나이 / 전화번호를 적어주세요</PanelDetail>
+              </PanelInfo>
+            </PanelTop>
+          </Panel>
+        </Box>
+
+        <Box>
+          <div style={{fontSize: '14px'}}>확인 목록이 없습니다.</div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </Box>
+      </Layout>
+    </div>
+  );
+};
+
+const Golayout = keyframes`
+  0$ {
+    opacity: 0;
+    left: -20px;
   }
-}
+  100% {
+    opacity: 1;
+    left: 0;
+  }
+`;
 
 const Layout = styled.div`
   min-width: 1200px;
@@ -79,18 +94,18 @@ const Box = styled.div`
   width: 448px;
   margin-right: 8px;
   padding: 16px;
-  border: 1px solid $line-color;
+  border: 1px solid #ddd;
   border-radius: 5px;
-  background: $white-color;
+  background: white;
   box-sizing: border-box;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.08);
-  animation: golayout 0.35s;
+  animation: ${Golayout} 0.35s;
   height: max-content;
 `;
 
-const Panel = styled.div`
+const Panel = styled.button`
   position: relative;
-  width: 361px;
+  width: 405px;
   flex-basis: 256px;
   flex-grow: 1;
   height: 76sp;
@@ -104,11 +119,17 @@ const Panel = styled.div`
   transition: 0.35s;
   color: #495057;
   display: inline-block;
+  background: white;
 
-  :hover {
-    box-shadow: 0 6px 24px rgba(0, 0, 0, 0.16);
+  &:hover {
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.12);
     transition: 0.35s;
-    background: darken($white-color, 2%);
+  }
+  &:focus {
+    background-color: #495057;
+    color: white;
+    border: 0;
+    outline: 0;
   }
 `;
 
